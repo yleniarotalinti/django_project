0c85862caf204f81693284363df4cab0cf97fbfa
Rotalinti <Ylenia.Rotalinti@mhra.gov.uk>
2024-05-31 10:33:22 +0100

    Created my first view

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# view: function that takes a web request and returns a web response
+def say_hello(request):
+    return HttpResponse("Hello, Django!")
