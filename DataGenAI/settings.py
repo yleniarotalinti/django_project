0f396a0f83dc200ea3074474d0e1613bad97b235
ylenia.rotalinti <ylenia.rotalinti@gmail.com>
2024-06-13 12:28:11 +0100

    Change setting to allow the correct visualisation of the admin page

--- DataGenAI/settings.py
+++ DataGenAI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sessions',
     "debug_toolbar",
     'playground',
 ]
