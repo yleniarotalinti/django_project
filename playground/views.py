a7ed568231a33b9108d952b6b4a0728c9896d82a
ylenia.rotalinti <ylenia.rotalinti@gmail.com>
2024-06-13 12:27:22 +0100

    Created a new model. Populated the model with some records and displayed the records in the page.

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Post
 
 # Create your views here.
 # view: function that takes a web request and returns a web response
 
 def show_my_name(request):
     # Pass a dictionary to the template to render the template with some data (in the html file you can access the data using the key)
-    return render(request, 'about.html')  
+    posts = Post.objects.all().order_by('-created_at')    #descending order
+    return render(request, 'about.html', {'posts': posts})  
+
