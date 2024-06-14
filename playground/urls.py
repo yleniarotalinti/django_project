636f0caabfebfcc7d67610aa5961b832b5aa2dfd
ylenia.rotalinti <ylenia.rotalinti@gmail.com>
2024-06-14 12:29:07 +0100

    Handle slugs dinamically

--- playground/urls.py
+++ playground/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+# in a way to avoid conflicts with other apps
+# now authomaticall the url will be playground/...
+# to call them in the html we will use {% url 'playground:posts_list' %}
+app_name = 'playground'
+
 # URL Configuration
 urlpatterns = [
-    path('', views.show_my_name)
+    path('', views.show_my_name, name="posts_list"),
+    # slug (after :) is a parameter that we pass to the view 
+    path('<slug:slug>', views.show_my_post, name="post_detail")
 ]
--- playground/views.py
+++ playground/views.py
@@ -9,5 +9,12 @@ from .models import Post
 def show_my_name(request):
     # Pass a dictionary to the template to render the template with some data (in the html file you can access the data using the key)
     posts = Post.objects.all().order_by('-created_at')    #descending order
-    return render(request, 'about.html', {'posts': posts})  
+    return render(request, 'about.html', {'posts': posts}) 
+
+def show_my_post(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'post_detail.html', {'post': post})
+
+
+
 
